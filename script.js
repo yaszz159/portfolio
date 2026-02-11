f05f1d6bce7d62b833cc2276afd4b20c5e67ff45
Zulaikha Yasmeena Assafik <zulaikhayasmeen09@gmail.com>
2026-02-11 08:44:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,9 @@ function toggleCard(card) {
     } else {
         hint.innerText = "Click to see more";
     }
+    function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
+    }
+
 }
